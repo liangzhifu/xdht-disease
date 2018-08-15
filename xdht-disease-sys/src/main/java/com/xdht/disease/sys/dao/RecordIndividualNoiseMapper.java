75c67876ea3ad2659580619e1ce24bc418d95fac
liangzhifu <1009568392@qq.com>
2018-08-15 13:50:10 +0800

    个体噪声暴露评估修改

--- xdht-disease-sys/src/main/java/com/xdht/disease/sys/dao/RecordIndividualNoiseMapper.java
+++ xdht-disease-sys/src/main/java/com/xdht/disease/sys/dao/RecordIndividualNoiseMapper.java
@@ -2,15 +2,6 @@ package com.xdht.disease.sys.dao;
 
 import com.xdht.disease.common.core.Mapper;
 import com.xdht.disease.sys.model.RecordIndividualNoise;
-import com.xdht.disease.sys.vo.response.RecordIndividualNoiseResponse;
-
-import java.util.List;
 
 public interface RecordIndividualNoiseMapper extends Mapper<RecordIndividualNoise> {
-
-    /**
-     * 查询echarts信息
-     * @return 返回结果
-     */
-    List<RecordIndividualNoiseResponse> selectRecordIndividualNoiseEcharsDetail();
 }
