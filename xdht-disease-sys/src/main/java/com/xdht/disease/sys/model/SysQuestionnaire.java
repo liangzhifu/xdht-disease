82009b57872558987b0b590029015cbb5c83d7f2
liumeng <timegarden1129@outlook.com>
2018-08-23 09:09:33 +0800

    项目编码组合 调查表编号 绑定输出

--- xdht-disease-sys/src/main/java/com/xdht/disease/sys/model/SysQuestionnaire.java
+++ xdht-disease-sys/src/main/java/com/xdht/disease/sys/model/SysQuestionnaire.java
@@ -5,13 +5,17 @@ import lombok.Data;
 import java.util.Date;
 import javax.persistence.*;
 
-@Data
+/*@Data*/
 @Table(name = "sys_questionnaire")
 public class SysQuestionnaire {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    /*
+    * 调查编号
+    */
+    @Column(name = "questionnaire_num")
+    private Long questionnaireNum;
     /**
      * 调查表名称
      */
@@ -54,4 +58,78 @@ public class SysQuestionnaire {
     @Column(name = "update_date")
     private Date updateDate;
 
+    public Long getQuestionnaireNum() {
+        return questionnaireNum;
+    }
+
+    public void setQuestionnaireNum(Long questionnaireNum) {
+        this.questionnaireNum = questionnaireNum;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestionnaireName() {
+        return questionnaireName;
+    }
+
+    public void setQuestionnaireName(String questionnaireName) {
+        this.questionnaireName = questionnaireName;
+    }
+
+    public String getQuestionnaireAlias() {
+        return questionnaireAlias;
+    }
+
+    public void setQuestionnaireAlias(String questionnaireAlias) {
+        this.questionnaireAlias = questionnaireAlias;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Long createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(Long updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 }
